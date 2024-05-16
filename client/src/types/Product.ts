01af9bc9984e3d2767679acf895779b634ad236c
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-16 23:50:50 +0300

    add product data type

---
+++ client/src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+    name:string;
+    slug:string;
+    image:string;
+    description:string;
+    category:string;
+    brand:string;
+    countInStock:number;
+    rating:number;
+    numReviews:number;
+    price:number;
+}
