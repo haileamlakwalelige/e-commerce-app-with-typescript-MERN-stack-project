bd7e51650c2a3444548e851117ae29ba8d04d3f2
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-16 23:51:04 +0300

    add items in app file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
+import { sampleProducts } from "./data";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col justify-center items-center">
+      <header>Header</header>
+      <main className="w-screen flex flex-col justify-center items-center">
+        <ul>
+          {
+            sampleProducts.map((product)=>(
+              <li key={product.slug} className="py-3 flex gap-2 flex-col">
+                <img src={product.image} alt={product.name} className="max-h-[300px] max-w-[300px] "/>
+                <h1>{product.name}</h1>
+                <p>${product.price}</p>
+              </li>
+            ))
+          }
+        </ul>
+      </main>
+    </div>
   )
 }
