1772bcbfdcd1ddcabc0b1e43386329209e7b3fcc
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-18 01:29:49 +0300

    add data to the app section and make the app section as a top section or file.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,29 @@
-import { sampleProducts } from "./data";
+import { Link, Outlet } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <header>Header</header>
-      <main className="w-screen flex flex-col justify-center items-center">
-        <ul>
-          {
-            sampleProducts.map((product)=>(
-              <li key={product.slug} className="py-3 flex gap-2 flex-col">
-                <img src={product.image} alt={product.name} className="max-h-[300px] max-w-[300px] "/>
-                <h1>{product.name}</h1>
-                <p>${product.price}</p>
-              </li>
-            ))
-          }
-        </ul>
+    <div className="flex flex-col justify-center items-center overflow-x-hidden">
+      <header className="p-2 bg-slate-800 text-white ">
+        <div className="flex gap-10 flex-row justify-between items-center w-screen sm:px-4 md:px-8 lg:px-16 p-2">
+          <div>
+            Haileopia
+          </div>
+          <div className="flex gap-4 justify-center items-center">
+            <Link to='/cart'>
+              <p>Cart</p>
+            </Link>
+            <Link to='/sign-in'>
+              <p>Sign In</p>
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main className="w-screen flex flex-col justify-center items-center my-6">
+        <Outlet />
       </main>
+      <footer>
+        <p>All right reserved!</p>
+      </footer>
     </div>
   )
 }
