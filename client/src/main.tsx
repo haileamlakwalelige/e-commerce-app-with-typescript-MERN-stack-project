6311d5f436a4a915cff987dd3237dbec36cdca03
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-18 01:30:11 +0300

    adding react router dom version 6

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
+import Home from './pages/Home.tsx';
+import ProductPage from './pages/ProductPage.tsx';
+// import { Product } from './types/Product';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<Home />} />   
+         <Route path="product/:slug" element={<ProductPage />} />
+      {/* ... etc. */}
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
